b14cdba9d68fcbc76798dfec92cbdb814c91e0f3
Cj-bc <cj.bc-sd@outlook.jp>
2022-07-02 17:29:48 +0900

    Change: workdir/vocoding> make 'convert_mono' more general
    It can recieve any function that manipulate f0/sp

--- workdir/vocoding.py
+++ workdir/vocoding.py
@@ -12,18 +12,22 @@ OUT_FILE = os.path.join(os.path.dirname(__file__), "converted.wav")
 
 data, samplerate = sf.read(INPUT_FILE)
 
-def convert_mono(mono_audio, samplerate):
+def identical(a):
+        return a
+
+def convert_mono(mono_audio, samplerate: float,
+                 f0_converter=identical, sp_converter=identical):
     # https://stackoverflow.com/questions/26778079/valueerror-ndarray-is-not-c-contiguous-in-cython
 	f0, sp, ap = pw.wav2world(mono_audio.copy(order='C'), samplerate)
 	converted_sp = np.zeros_like(sp)
 
 	for f in range(converted_sp.shape[1]):
-		converted_sp[:, f] = sp[:, int(f/1.2)]
+		converted_sp[:, f] = sp[:, sp_converter(f)]
 
-	return pw.synthesize(f0, converted_sp, ap, samplerate)
+	return pw.synthesize(f0_converter(f0), converted_sp, ap, samplerate)
 
-ch1 = convert_mono(data[:, 0], samplerate)
-ch2 = convert_mono(data[:, 1], samplerate)
+ch1 = convert_mono(data[:, 0], samplerate, lambda f0: f0, lambda sp_v: int(sp_v/1.2))
+ch2 = convert_mono(data[:, 1], samplerate, lambda f0: f0, lambda sp_v: int(sp_v/1.2))
 
 result = np.dstack([ch1, ch2])[0,:,:]
 sf.write(OUT_FILE, result, samplerate)
